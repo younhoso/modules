fa89289a5ad1df6e5a3ba2ea6ffe88d4620136f6
소윤호 <thdbsgh3443@naver.com>
2021-02-03 00:42:55 +0900

    swiper_custom pagination, paused 옵션 추가 기능 작업중

--- src/js/Custom/app.js
+++ src/js/Custom/app.js
@@ -39,7 +39,7 @@ export default class Motion extends Swiper {
         super(el, ops);
         this.el = el;
         this.passedParams = ops;
-        this.store = {ele: null, slideEle: null, pageEl: null, dotEle: null}
+        this.store = {ele: null, slideEle: null, pageEl: null, dotEles: null, pausedValue: null}
         this.current = null;
         this.#initMotion();
     };
@@ -47,11 +47,12 @@ export default class Motion extends Swiper {
      * Motion의 초기화 작업 
      */
     #initMotion() {
-        const {pagination} = this.passedParams
+        const {pagination, paused} = this.passedParams
         this.store.ele = Array.from(_tr(this.el));
         this.store.slideEle = Array.from(_tr(this.el).find('.swiper-slide'));
         this.store.pageEl = _tr(pagination.el);
-        if(!pagination){ return false } else { this.store.dotEle = Array.from(_tr(pagination.el).find('.swiper-pagination-bullet')); }
+        if(!pagination){ return false } else { this.store.dotEles = Array.from(_tr(pagination.el).find('.swiper-pagination-bullet')); }
+        if(!paused){ return false } else { this.store.pausedValue = this.passedParams.paused }
         this.#pageOnCircle();
     };
 
@@ -79,18 +80,43 @@ export default class Motion extends Swiper {
      * dot circle 기능 구현
      */
     #pageOnCircle() {
-        const {pageEl, dotEle} = this.store;
-        const {autoplay } = this.passedParams
-        const {dotcircle, pageinationStop, circleWidth, circleHeight, strokeColor, strokeWidth, dotColor} = this.passedParams.pagination
+        const {dotEles} = this.store;
+        const {autoplay , paused} = this.passedParams
+        const {dotcircle, circleSize, strokeColor, strokeWidth, dotColor} = this.passedParams.pagination
+        const {pausedEl, size, strokePausedColor, strokePausedWidth, dotPausedColor} = this.store.pausedValue
 
-        const playPauseTemplate = () => `<button class='playpause'></button>`
-
+        const stopTemplate = () => {
+            return `
+                <a class="swiper-btn-stop paused active" href="#0">
+                    <svg width="${size}" height="31" viewBox="0 0 31 31">
+                        <g fill="none" fill-rule="evenodd">
+                            <circle cx="15.5" cy="15.5" r="12.5" stroke="${strokePausedColor}" stroke-width="${strokePausedWidth}"/>
+                            <g stroke="#333" stroke-linecap="round" stroke-width="2">
+                                <path d="M18 12v7M13 12v7"/>
+                            </g>
+                        </g>
+                    </svg>
+                </a>
+            `;
+        };
+        const playTemplate = () => {
+            return `
+                <svg width="31" height="31" viewBox="0 0 31 31">
+                    <g fill="none" fill-rule="evenodd">
+                        <circle cx="15.5" cy="15.5" r="12.5" stroke="#DDD"/>
+                        <path fill="#333" d="M20.213 15.937l-7.47 4.15A.5.5 0 0 1 12 19.65v-8.3a.5.5 0 0 1 .743-.437l7.47 4.15a.5.5 0 0 1 0 .874z"/>
+                    </g>
+                </svg>
+            `;
+        };
         const dotCircleTemplate = () => {
             return `
-                <svg class="circle" width="${circleWidth}" viewBox="0 0 16 16">
-                    <circle class="strok" cx="8" cy="8" r="6.5" fill="none" transform="rotate(-90 8 8)" stroke="${strokeColor}" stroke-opacity="1" stroke-width="${strokeWidth}px"></circle>
-                    <circle class="dot" cx="8" cy="8" r="3" fill="${dotColor}"></circle>
-                </svg>
+                <a href="#0">
+                    <svg class="circle" width="${circleSize}" viewBox="0 0 16 16">
+                        <circle class="strok" cx="8" cy="8" r="6.5" fill="none" transform="rotate(-90 8 8)" stroke="${strokeColor}" stroke-opacity="1" stroke-width="${strokeWidth}px"></circle>
+                        <circle class="dot" cx="8" cy="8" r="3" fill="${dotColor}"></circle>
+                    </svg>
+                </a>
             `;
         };
         
@@ -103,16 +129,25 @@ export default class Motion extends Swiper {
             this.#active(self); // 클래스 활성화
         };
         
-        pageinationStop && pageEl.append( playPauseTemplate() )
+        paused && _tr(pausedEl).html(stopTemplate());
 
         /** 정지버튼 | 재생버튼 클릭 이벤트 */
-        _tr('.playpause').on('click', (e) => {
-            this.autoplay.stop();
-            _tr('.swiper-pagination-bullet-active .strok').addClass('paused')
+        _tr('.paused').on('click', (e) => {
+            if(_tr(e.currentTarget).hasClass('active')) {
+                _tr(e.currentTarget).removeClass('active');
+                _tr(e.currentTarget).html(playTemplate());
+                _tr('.swiper-pagination-bullet-active .strok').addClass('paused');
+                this.autoplay.stop();
+            } else {
+                _tr(e.currentTarget).addClass('active');
+                _tr(e.currentTarget).html(stopTemplate());
+                _tr('.swiper-pagination-bullet-active .strok').removeClass('paused');
+                this.autoplay.start();
+            }
         });
 
-        dotcircle && dotEle.reduce((acc, cur, idx, arr) => {
-            _tr(cur).css('background', 'transparent')
+        dotcircle && dotEles.reduce((acc, cur, idx, arr) => {
+            _tr(cur).css({width:"auto",height:"auto",background:"transparent"});
             cur.innerHTML = dotCircleTemplate();
             /** dot클릭 이벤트 */
             _tr(cur).on('click', (e) => {
