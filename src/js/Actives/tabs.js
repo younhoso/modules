4179311933bcdf00270b9af0d28cc933f4781b29
소윤호 <thdbsgh3443@naver.com>
2021-02-09 22:52:27 +0900

    tabs.js에서 valueChang, lineAnime 메소드 수정

--- src/js/Actives/tabs.js
+++ src/js/Actives/tabs.js
@@ -24,44 +24,50 @@ export default class Tabs extends Actives {
         this.initHandler();
     };
     /**
-     * 특정 엘리먼트 속성 값을 바꿔주는 메소드
+     * 특정 엘리먼트 속성 값을 바꿔주는 메소드.
      * @param {HTMLElement} this event handler.
      */
-    ValueChang (self) {
+    valueChang(self) {
         const { findTargets, prop, unactiveValue, activeValue} = this.el.addChild;
         _tr(self).find(findTargets).attr(prop, activeValue); //속성 활성화    
         _tr(self).siblings().find(findTargets).attr(prop, unactiveValue); //속성 이전 활성화
-    }
+    };
     /**
-     * 특정 조건에만 실행하는 메소드
+     *  Tabs Menu UI중에서 라인선이 클릭한 곳으로 좌,우로 애니메이션 되기 위한 메소드. 
+     */
+    lineAnime() {
+        const { lineAnime } = this.el;
+        const LINETEMPLATE = () => `<i class="tr_line_item"></i>`;
+
+        const EL = _tr(lineAnime.targets);
+        EL.html(LINETEMPLATE());
+    };
+
+    /**
+     * 특정 조건에만 실행만하는 메소드.
      */
     initHandler() {
         const { targets, addClassName, lineAnime } = this.el;
         this.store.target = _tr(targets);
         this.store.addClassName = addClassName;
         
-        const lineTemplate = () => `<i class="tr_line_item"></i>`;
-
-        const line = _tr(lineAnime.targets);
-        line.html(lineTemplate());
-        
         const handler = (self) => {
             this.current && super.unactive(this.current); //클래스 비활성화
             super.active(self); // 클래스 활성화
             this.store.idx = _tr(targets).indexOf(self)
-            this.el.addChild && this.ValueChang(self);
+            this.el.addChild && this.valueChang(self);
         };
 
-        (() => {
-           const w = this.store.target[0].clientWidth;
-            _tr('.tr_line_item').css({'width': w + 'px'})
+        lineAnime && this.lineAnime();
+        const w = this.store.target[0].clientWidth;
+        _tr('.tr_line_item').css({'width': w + 'px'})
 
-            this.store.target.on('click', (e) => {
-                e.preventDefault();
-                handler(e.currentTarget)
-                TweenMax.to('.tr_line_item', {x: w * this.store.idx})
-            });
-        })();
+        this.store.target.on('click', (e) => {
+            e.preventDefault();
+            handler(e.currentTarget)
+            TweenMax.to('.tr_line_item', {x: w * this.store.idx})
+        });
+
         handler(this.store.target[0])
     };
 };
