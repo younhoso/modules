5b38c39e0564cfc01cb5d7dc1aa116bd7205d7f8
소윤호 <thdbsgh3443@naver.com>
2021-03-05 01:58:38 +0900

    accordions 기능 개선 및 basic.js autoplay 코드 개선

--- src/js/Actives/accordions.js
+++ src/js/Actives/accordions.js
@@ -1,3 +1,4 @@
+import Actives from '../Helpers/basic';
 import { _tr } from '../Helpers/DomApi.js';
 import { TweenMax } from 'gsap/all';
 
@@ -9,36 +10,58 @@ export default class Accordions {
         targets: '.item',
         event: 'click',                 // 이벤트 mouseenter와, click 2가지를 옵션으로 넣을수 있습니다.
         addClassName: 'active',         // 넣고 싶은 클랙스 명
-        firstItemActive: true,          // 첫번째 아이템을 활성화 할건지 여부체크 true or false 
+        firstItemActive: true,          // 첫번째 아이템을 활성화 할건지 여부체크 true or false
+        autoplay: 800,
+        loop: true,
       });
     */
   constructor(el) {
     this.el = el;
-    this.store = { ele: null, eleSib: null, self: null };
+    this.store = { ele: null, eleSib: null, self: null, targetIdx: null, gsapSelf: null, gsapSelfSib: null };
     this.initHandler();
   }
+  activeItem(self) {
+    this.store.gsapSelf = TweenMax.to(self, 0.4, { height: 'auto' });
+  }
+
+  unActiveItem(selfSib) {
+    this.store.gsapSelfSib = TweenMax.to(selfSib, 0.4, { height: '0' });
+  }
+
   /** 특정 조건에만 실행하는 메소드. */
   initHandler() {
-    const { targets, event, firstItemActive } = this.el;
+    const { targets, event, firstItemActive, autoplay, loop } = this.el;
     const target = _tr(targets);
     this.store.eleSib = target.siblings().find('.tr_accordion');
     this.store.ele = target.find('.tr_accordion');
 
+    /**  이벤트 핸들러 함수(즉시 실행) */
     (() => {
-      /**  이벤트 핸들러 함수(즉시 실행) */
       target.reduce((acc, cur, idx, src) => {
+        this.store.targetIdx = idx;
         cur.addEventListener(event, e => {
           e.preventDefault();
           e.stopPropagation();
           const _selfSib = _tr(e.currentTarget).siblings().find('.tr_accordion');
           const _self = _tr(e.currentTarget).find('.tr_accordion');
 
-          TweenMax.to(_selfSib, { height: '0' });
-          TweenMax.to(_self, { height: 'auto' });
+          this.unActiveItem(_selfSib);
+          this.activeItem(_self);
         });
       }, 0);
 
-      firstItemActive ? (this.store.eleSib.css('height', 0), this.store.ele.eq(0).css('height', 'auto')) : this.store.ele.css('height', 0);
+      if (firstItemActive) {
+        this.store.eleSib.css('height', 0);
+        this.activeItem(this.store.ele[0]);
+      } else if (autoplay) {
+        this.store.eleSib.css('height', 0);
+        Actives.autoplay(this.store.ele, autoplay, loop, i => {
+          this.unActiveItem(this.store.eleSib);
+          this.activeItem(this.store.ele[i]);
+        });
+      } else {
+        this.store.eleSib.css('height', 0);
+      }
     })();
   }
 }
--- src/js/Helpers/basic.js
+++ src/js/Helpers/basic.js
@@ -15,7 +15,7 @@ export default class Actives {
   /**
    * 기본적인 인덱스번째 실행
    */
-  indexDefault() {
+  static indexDefault() {
     const { targets } = this.el;
     if (this.store.currentIndex < 0) this.store.currentIndex = 0; //총 길이보다 작으면 멈춘다.
     if (this.store.currentIndex > this.store.length) this.store.currentIndex = this.store.length; //총 길이보다 크면 멈춘다.
@@ -26,13 +26,18 @@ export default class Actives {
   /**
    * 인덱스번째 무한 반복 실행
    */
-  indexLoop() {
-    const { targets } = this.el;
-    if (this.store.currentIndex < 0) this.store.currentIndex = this.store.length; //총 길이보다 작으면 다시 가장 마지막번째가 된다.
-    if (this.store.currentIndex > this.store.length) this.store.currentIndex = 0; //총 길이보다 크면 다시 0번째로 된다.
+  static indexLoop(target, idx) {
+    // const { targets } = this.el;
+    let length = 0,
+      currentIndex = 0;
 
-    const arrItem = Array.from(document.querySelectorAll(targets));
-    this.store.length = arrItem.length - 1;
+    if (idx < 0) currentIndex = length; //총 길이보다 작으면 다시 가장 마지막번째가 된다.
+    if (idx > length) currentIndex = 0; //총 길이보다 크면 다시 0번째로 된다.
+
+    const arrItem = Array.from(target);
+    length = arrItem.length - 1;
+    console.log(arrItem);
+    return currentIndex;
   }
   /**
    * 활성화 관한 메소드
@@ -56,22 +61,17 @@ export default class Actives {
     this.current && item.classList.remove(addClassName); //기본적인 class 삭제 기능
   }
   /**
-   * 자동실행, loop 관한 메소드
+   * 자동실행, loop 관한 메소드(static 메소드).
    * @type {object}
-   * @param {eventcurrent}
+   * @param {Element, Number, boolean, callback}
    */
-  autoplay(item) {
-    const { autoplay, loop } = this.el;
+  static autoplay(item, autoplay, loop, func) {
     let timeId;
     let n = 0;
 
     const autos = () => {
-      if (loop && n >= item.length) {
-        n = 0;
-      }
-      if (n >= item.length) return false;
-      this.active(item[n]);
-      n++;
+      if (loop && n >= item.length) n = 0;
+      n >= item.length ? clearInterval(timeId) : (func(n), n++);
     };
 
     timeId = setInterval(autos, autoplay);
