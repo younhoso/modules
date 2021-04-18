1913c2056ce3da4c2575d13e48e5ba9270152dc0
TripleLab <triplexlab88@naver.com>
2021-04-18 23:16:54 +0900

    ♻️ 코드 리팩토링

--- src/js/Actives/tabs.js
+++ src/js/Actives/tabs.js
@@ -7,7 +7,7 @@ export default class Tabs extends Actives {
      * @param {Options | object}
      * @example
      * const my = new tr.Tabs({
-            targets: '.tab_js',
+            targets: '.tr_tab',
             addClassName: 'actives',      // 넣고 싶은 클랙스 명
             firstItemActive: true,        // 첫번째 아이템을 활성화 할건지 여부체크 true or false
             addChild: {
@@ -23,47 +23,64 @@ export default class Tabs extends Actives {
     this.store = { idx: null, target: null, addClassName: null, PrevValue: null };
     this.initHandler();
   }
-  /**
-   * 특정 엘리먼트 속성 값을 바꿔주는 메소드.
-   * @param {HTMLElement} this event handler.
-   */
-  valueChang(self) {
-    const { findTargets, prop, unactiveValue, activeValue } = this.el.addChild;
-    _tr(self).find(findTargets).attr(prop, activeValue); //속성 활성화
-    _tr(self).siblings().find(findTargets).attr(prop, unactiveValue); //속성 이전 활성화
-  }
-  /** Tabs Menu UI중에서 라인선이 클릭한 곳으로 좌,우로 애니메이션 되기 위한 메소드.  */
-  lineAnime() {
-    const { lineAnime } = this.el;
-    const LINETEMPLATE = () => `<i class="tr_line_item"></i>`;
 
-    const EL = _tr(lineAnime.targets);
-    EL.html(LINETEMPLATE());
+  /** Tabs 메뉴들에 관한 템플릿을 createDocumentFragment라는 가상문서에 저장하는 메소드. */
+  menuTemplate() {
+    const { targets, menuName, tabLine } = this.el
+    const docFrag = document.createDocumentFragment();
+    const tabsEle = document.querySelector(targets);
+
+    const ul = document.createElement('ul');
+    ul.classList.add('tr_tab_inner')
+    docFrag.appendChild(ul);
+
+    menuName.forEach(function(item, idx){
+      const li = document.createElement('li');
+      const a = document.createElement('a');
+      li.classList.add('tr_tab_item');
+      a.setAttribute('href', `#sec-tabs-${idx + 1}`)
+      a.innerHTML = item;
+
+      li.appendChild(a);
+      ul.appendChild(li);
+      docFrag.appendChild(ul);
+    });
+
+    const div = document.createElement('div');
+    const i = document.createElement('i');
+    div.classList.add('line', 'tr_line');
+    i.classList.add('tr_line_item');
+
+    tabLine && docFrag.appendChild(div);
+    docFrag.appendChild(i);
+    div.appendChild(i);
+    
+    tabsEle.appendChild(docFrag);
   }
 
   /** 특정 조건에만 실행만하는 메소드. */
   initHandler() {
-    const { targets, addClassName, lineAnime } = this.el;
-    this.store.target = _tr(targets);
+    this.menuTemplate()
+    const { targets, addClassName, firstItemActive } = this.el;
+    this.store.target = _tr(targets).find('.tr_tab_item');
     this.store.addClassName = addClassName;
 
-    const handler = self => {
-      this.current && super.unactive(this.current); //클래스 비활성화
+    const handler = (self) => {
+      super.unactive(this.current); //클래스 비활성화
       super.active(self); // 클래스 활성화
-      this.store.idx = _tr(targets).indexOf(self);
-      this.el.addChild && this.valueChang(self);
+      this.store.idx = this.store.target.indexOf(self);
     };
 
-    lineAnime && this.lineAnime();
     const w = this.store.target[0].clientWidth;
-    _tr('.tr_line_item').css({ width: w + 'px' });
 
-    this.store.target.on('click', e => {
+    this.store.target.on('click', (e) => {
       e.preventDefault();
       handler(e.currentTarget);
-      anis('.tr_line_item', 0.4, { x: w * this.store.idx });
+      firstItemActive || _tr('.tr_line_item').css({ width: w + 'px' });
+      anis('.tr_line_item', 0.4, { x: (w * this.store.idx) });
     });
 
-    handler(this.store.target[0]);
+    firstItemActive && _tr('.tr_line_item').css({ width: w + 'px' });
+    firstItemActive && handler(this.store.target[0]);
   }
 }
