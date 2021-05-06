b803ab1639394df3637fe2ada7c55047ed339d80
소윤호 <thdbsgh3443@naver.com>
2021-05-06 09:51:26 +0900

    ⏪  TabsScroll 기능 추가 하지 않을생각 입니다.

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@
 import Accordions from '@/js/Actives/accordions';
 import Tabs from '@/js/Actives/tabs';
 import Tabprops from '@/js/Actives/tabprops';
-import TabsScroll from '@/js/Actives/tabsScroll';
 import ToggleDynamic from '@/js/Actives/toggleDynamic';
 
 // Breakpoints
@@ -37,7 +36,6 @@ export {
   Accordions,
   Tabs,
   Tabprops,
-  TabsScroll,
   ToggleDynamic,
   BreakPoints,
   comma,
--- src/js/Actives/index.js
+++ src/js/Actives/index.js
@@ -2,7 +2,6 @@
 import Accordions from '@/js/Actives/accordions';
 import Tabs from '@/js/Actives/tabs';
 import Tabprops from '@/js/Actives/tabprops';
-import TabsScroll from '@/js/Actives/tabsScroll';
 import ToggleDynamic from '@/js/Actives/toggleDynamic';
 
-export { Accordions, Tabs, Tabprops, TabsScroll, ToggleDynamic };
+export { Accordions, Tabs, Tabprops, ToggleDynamic };
